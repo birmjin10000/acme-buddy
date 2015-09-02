9bce6c47c0b06a85bd61a8b785ae9ba55110e324
birmjin10000 <yinbirmjin@gmail.com>
2015-09-02 11:05:45 +0900

    [acme-webstore] Sync to whole library version to the newest

--- acme-webstore/project.clj
+++ acme-webstore/project.clj
@@ -4,21 +4,21 @@
   :min-lein-version "2.0.0"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.2.1"]
-                 [ring "1.3.1"]
-                 [ring/ring-jetty-adapter "1.3.1"]
-                 [ring/ring-json "0.3.1"]
-                 [ring/ring-headers "0.1.1"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [compojure "1.4.0"]
+                 [ring "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-json "0.4.0"]
+                 [ring/ring-headers "0.1.3"]
                  [hiccup "1.0.5"]
-                 [clj-http "1.0.1"]
-                 [ch.qos.logback/logback-classic "1.1.2"]
-                 [buddy/buddy-sign "0.3.0"]]
+                 [clj-http "2.0.0"]
+                 [ch.qos.logback/logback-classic "1.1.3"]
+                 [buddy/buddy-sign "0.6.1"]]
   :ring {:handler acme-webstore.web/app
          :port 6002}
-  :profiles {:dev {:plugins [[lein-ring "0.8.13"]]
+  :profiles {:dev {:plugins [[lein-ring "0.9.6"]]
                    :test-paths ^:replace []}
-             :test {:dependencies [[midje "1.6.3"]]
+             :test {:dependencies [[midje "1.7.0"]]
                     :plugins [[lein-midje "3.1.3"]]
                     :test-paths ["test"]
                     :resource-paths ["test/resources"]}})
